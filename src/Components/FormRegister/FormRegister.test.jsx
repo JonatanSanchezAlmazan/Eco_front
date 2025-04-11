69248eec397314d9dc5d76224398f78e9bfcf743
Jonatan Sanchez <jonatansan1@hotmail.com>
2025-04-11 09:57:36 +0200

    newcommit

--- src/Components/FormRegister/FormRegister.test.jsx
+++ src/Components/FormRegister/FormRegister.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import FormRegister from './FormRegister';
 import { vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
@@ -34,7 +34,7 @@ describe('FormRegister', () => {
     render(<FormRegister navigate={mockNavigate} dispatch={mockDispatch} />);
 
     const emailInput = screen.getByLabelText(/email/i);
-    fireEvent.change(emailInput, { target: { value: 'incorrecto' } });
+    userEvent.type(emailInput, 'pruebadominio.com');
 
     const submitButton = screen.getByRole('button', { name: /registrarse/i });
     userEvent.click(submitButton);
@@ -42,17 +42,6 @@ describe('FormRegister', () => {
     expect(await screen.findByText(/el email no tiene un formato válido/i)).toBeInTheDocument();
   });
 
-  it('muestra un error si la contraseña no cumple con los requisitos', async () => {
-    render(<FormRegister navigate={mockNavigate} dispatch={mockDispatch} />);
-
-    const passwordInput = screen.getByLabelText(/contraseña/i);
-    fireEvent.change(passwordInput, { target: { value: '123' } });
-
-    const submitButton = screen.getByRole('button', { name: /Registrarse/i });
-    userEvent.click(submitButton);
-
-    expect(await screen.findByText(/la contraseña debe tener al menos 8 caracteres/i)).toBeInTheDocument();
-  });
   it('muestra un error si no se selecciona un rol', async () => {
     render(<FormRegister navigate={mockNavigate} dispatch={mockDispatch} />);
 
@@ -61,13 +50,14 @@ describe('FormRegister', () => {
     const passwordInput = screen.getByLabelText(/contraseña/i);
     const rolSelect = screen.getByLabelText(/rol/i);
 
-    fireEvent.change(nameInput, { target: { value: 'Juan Perez' } });
-    fireEvent.change(emailInput, { target: { value: 'juan@example.com' } });
-    fireEvent.change(passwordInput, { target: { value: 'Contraseña123!' } });
-    fireEvent.change(rolSelect, { target: { value: 'Usuario' } });
+    userEvent.type(nameInput, 'Juan Perez');
+    userEvent.type(emailInput, 'juan@example.com');
+    userEvent.type(passwordInput, 'Contraseña123!');
+
+    fireEvent.change(rolSelect, { target: { value: '' } });
 
     const submitButton = screen.getByRole('button', { name: /registrarse/i });
-    fireEvent.click(submitButton);
+    userEvent.click(submitButton);
 
     expect(await screen.findByText(/el rol es requerido/i)).toBeInTheDocument();
   });
